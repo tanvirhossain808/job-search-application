86d664b2f0d759231689d417e9fdd963ffae1f98
tanvirHossain10100 <tanvirhossan528@gmail.com>
2024-01-01 02:03:48 +0600

    add job catogories

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,15 @@ import bannerPic from "./../../assets/banner/bnr.png";
 import { CiSearch } from "react-icons/ci";
 import { CiLocationOn } from "react-icons/ci";
 import { NavLink } from "react-router-dom";
-
+import img1 from "./../../assets/jobcategory/image 2.png";
+import img2 from "./../../assets/jobcategory/image 3.png";
+import img3 from "./../../assets/jobcategory/image 4.png";
+import img4 from "./../../assets/jobcategory/image 5.png";
+import img5 from "./../../assets/jobcategory/image 6.png";
+import img6 from "./../../assets/jobcategory/image 7.png";
+import img7 from "./../../assets/jobcategory/image 8.png";
+import img8 from "./../../assets/jobcategory/image 9.png";
+import arrow from "./../../assets/jobcategory/Arrow 1.svg";
 export const Home = () => {
   return (
     <>
@@ -52,6 +60,127 @@ export const Home = () => {
             <button>See current Jobs</button>
           </NavLink>
         </div>
+      </div>
+      <div className="explorejobsCategory">
+        <div className="exploreJobs">
+          <div className="jobCategory">
+            <div>
+              <img src={img1} alt="" />
+              <h3>Design</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="jobCategory">
+            <div>
+              <img src={img2} alt="" />
+              <h3>Sales</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="jobCategory">
+            <div>
+              <img src={img3} alt="" />
+              <h3>Marketing</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="jobCategory">
+            <div>
+              <img src={img4} alt="" />
+              <h3>Finance</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="jobCategory">
+            <div>
+              <img src={img5} alt="" />
+              <h3>Finance</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="jobCategory">
+            <div>
+              <img src={img6} alt="" />
+              <h3>Finance</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="jobCategory">
+            <div>
+              <img src={img7} alt="" />
+              <h3>Finance</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="jobCategory">
+            <div>
+              <img src={img8} alt="" />
+              <h3>Finance</h3>
+              <div>
+                <span>
+                  <p>256+ jobs available</p>
+                </span>
+                <span>
+                  <img src={arrow} alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="exploreJobs">
+          <div className="jobCategory">d</div>
+          <div className="jobCategory"></div>
+          <div className="jobCategory"></div>
+        </div> */}
       </div>
     </>
   );
