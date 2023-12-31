33f7964e12ff3f8c625b89a170d76acaf6c37445
tanvirHossain10100 <tanvirhossan528@gmail.com>
2023-12-31 22:27:38 +0600

    add responsive nave and homebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,60 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo/logo.png";
+import { IoCloseSharp } from "react-icons/io5";
+import { MdMenuOpen } from "react-icons/md";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [showMenue, setShowMenu] = useState(false);
   return (
-    // Home, Jobs, About, Contact, Favorite, Signup, Sign Out.
-
     <>
       <nav>
         <ul>
-          <li className="logo">
-            <img src={logo} alt="" />
-          </li>
-          <li>
-            <NavLink to="/">HOME</NavLink>
-          </li>
+          <span className="logoFeild">
+            <li className="logo">
+              <img src={logo} alt="" />
+            </li>
+          </span>
+          <span className={`navLInksBtn ${showMenue ? "showNav" : ""}`}>
+            <li>
+              <NavLink to="/">HOME</NavLink>
+            </li>
 
-          <li>
-            {" "}
-            <NavLink to="jobs">JOBS</NavLink>
-          </li>
-          <li>
-            <NavLink to="abouts">ABOUT</NavLink>
-          </li>
-          <li>
-            {" "}
-            <NavLink to="contact">CONTACT</NavLink>
-          </li>
-          <li>
-            <NavLink to="favorite">FAVORITE</NavLink>
-          </li>
-          <li>
-            {" "}
-            <NavLink to="signup">SIGN UP</NavLink>
-          </li>
-          <li>
-            {" "}
-            <NavLink to="SIGNIN">SIGNIN</NavLink>
-          </li>
-          <li>
-            {" "}
-            <NavLink to="SIGNOUT">SIGN OUT</NavLink>
-          </li>
+            <li>
+              <NavLink to="jobs">JOBS</NavLink>
+            </li>
+            <li>
+              <NavLink to="abouts">ABOUT</NavLink>
+            </li>
+            <li>
+              <NavLink to="contact">CONTACT</NavLink>
+            </li>
+            <li>
+              <NavLink to="favorite">FAVORITE</NavLink>
+            </li>
+            <li>
+              <NavLink to="signup">SIGN UP</NavLink>
+            </li>
+            <li>
+              <NavLink to="SIGNIN">SIGNIN</NavLink>
+            </li>
+            <li>
+              <NavLink to="SIGNOUT">SIGN OUT</NavLink>
+            </li>
+            {showMenue ? (
+              <li className="menuShowBtn" onClick={() => setShowMenu(false)}>
+                <IoCloseSharp />
+              </li>
+            ) : (
+              ""
+            )}
+          </span>
+          <span className="menubar menuShowBtn">
+            <li style={{ color: "black" }} onClick={() => setShowMenu(true)}>
+              <MdMenuOpen />
+            </li>
+          </span>
         </ul>
       </nav>
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,47 +2,55 @@ import "./Home.css";
 import bannerPic from "./../../assets/banner/bnr.png";
 import { CiSearch } from "react-icons/ci";
 import { CiLocationOn } from "react-icons/ci";
+import { NavLink } from "react-router-dom";
 
 export const Home = () => {
   return (
     <>
-      <div className="homeContainer">
-        <div className="landingContainer">
-          <div className="heroIntro">
-            <div className="heroText">
-              <h1>
-                Your Dream <br />
-                <span>Job Is Waiting.</span>{" "}
-              </h1>
-              <h2>10000+ Jobs</h2>
-              <div className="heroSearchInput">
-                <div className="searchBYKeyWord">
-                  <span>
-                    <CiSearch size={"30px"} color="#10B981" />
-                  </span>
-                  <input
-                    type="text"
-                    placeholder="Enter job title, keyword"
-                    name="jobTitle"
-                  />
-                </div>
-                <div className="searchByLocation">
-                  <span>
-                    <CiLocationOn size={"30px"} color="#10B981" />
-                  </span>
-                  <input
-                    type="text"
-                    placeholder="Search Location"
-                    name="location "
-                  />
-                 <button className="searchBtn">Search</button>
+      <div className="hero">
+        <div className="HeroContainer">
+          <div className="landingContainer">
+            <div className="heroIntro">
+              <div className="heroText">
+                <h1>
+                  Your Dream <br />
+                  <span>Job Is Waiting.</span>{" "}
+                </h1>
+                <h2>10000+ Jobs</h2>
+                <div className="heroSearchInput">
+                  <div className="searchBYKeyWord">
+                    <span>
+                      <CiSearch size={"30px"} color="#10B981" />
+                    </span>
+                    <input
+                      type="text"
+                      placeholder="Enter job title, keyword"
+                      name="jobTitle"
+                    />
+                  </div>
+                  <div className="searchByLocation">
+                    <span>
+                      <CiLocationOn size={"30px"} color="#10B981" />
+                    </span>
+                    <input
+                      type="text"
+                      placeholder="Search Location"
+                      name="location "
+                    />
+                    <button className="searchBtn">Search</button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="heroImg">
-              <img src={bannerPic} alt="" />
+              <div className="heroImg">
+                <img src={bannerPic} alt="" />
+              </div>
             </div>
           </div>
+        </div>
+        <div className="explorJobPageBtn">
+          <NavLink to={"jobs"}>
+            <button>See current Jobs</button>
+          </NavLink>
         </div>
       </div>
     </>
